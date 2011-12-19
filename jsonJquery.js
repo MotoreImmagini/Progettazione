a7830c670eac423da53e84f5fbc84f3d4f35d019
Giulia Fontanini <Giulia.Fonta@gmail.com>
2011-12-19 02:15:24 +0100

    inserimento pulsanti e color-pickup

--- jsonJquery.js
+++ jsonJquery.js
@@ -7,22 +7,35 @@ $(document).ready(function(){
 		var iCount = 0;
 		
 		var htmlString = [];
+		var htmlString2 = [];
+		var htmlString3 = [];
 		
 		$.each(data.items, function(i,item){
 		    
 		     if (iCount > iStart && iCount < (iStart + 13)) {
 		       
 		       htmlString += '<li><a href="' + item.media.m + '" target="_blank">';
-		       htmlString += '<img title="' + item.title + '" src="' + item.media.m + '"height="120px"';
+		       htmlString += '<img title="' + item.title + '" src="' + item.media.m + '" height=33% width=14%' ;
 		       htmlString += '" alt="'; htmlString += item.title + '" />';
 		       htmlString += '</a></li>';
 		       }
 		       iCount++;
+		});
 		
-		    });
-   
+		htmlString2 = '<div class="panel"><form><input type="text"></form></div><p class="flip">Search by tag</p>';
+		htmlString3 = '<div class="panel2"><form><input class="color" value="66ff00"></form></div><p class="flip2">Search by color</p></div>';
+		
 	    $('#lista').html(htmlString);
-	    $('#lista li').hide();
-		$('#lista li').show(2000);
+	    $('#tag-container').html(htmlString2);
+	    $('#color-container').html(htmlString3);
+	    $('#tag-container, #lista li, #color-container').hide();
+		$('#lista li, #tag-container, #color-container').show(2000);
+		
+		$(".flip").click(function(){
+			$(".panel").slideToggle("slow");
+		});
+		$(".flip2").click(function(){
+			$(".panel2").slideToggle("slow");
+		});
 	}
 });
