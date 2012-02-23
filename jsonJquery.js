ab91d53c79cf382bb902ecc8cb8779a2ee2625a2
Giulia Fontanini <Giulia.Fonta@gmail.com>
2012-02-23 14:44:31 +0100

    chiamata random: problemi con paginate

--- jsonJquery.js
+++ jsonJquery.js
@@ -1,10 +1,4 @@
-$(document).ready(function(){
-
-$.getJSON("http://shrek.micc.unifi.it:8080/daphnis/random?outfmt=JSON", displayImages);
-
 function displayImages(data) {
-var iStart = Math.floor(Math.random()*(8));
-var iCount = 0;
 
 var htmlString = [];
 
@@ -13,7 +7,7 @@ $.each(data.results, function(i,item){
 
 
 htmlString += '<li><a class="fancybox" title="' + item.filename + '" rel="group" href="' + item.link + '" target="_blank">';
-htmlString += '<img src="' + item.thumbURL + '" height=37%' ;
+htmlString += '<img src="' + item.link + '" height=37%' ;
 htmlString += '" alt="'; htmlString += item.title + '" />';
 htmlString += '</a></li>';
 
@@ -27,4 +21,3 @@ step:10
 });
 $('#lista a').show(2000);
 }
-});
